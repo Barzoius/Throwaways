1321c94fdff0cf2c58d810356ed5c34a80a80d19
Barzoius <mecca.rares@gmail.com>
2024-12-18 03:37:29 +0200

    boss fight
    super boring

--- Assets/Scripts/Bosses/BossBehaviour.cs
+++ Assets/Scripts/Bosses/BossBehaviour.cs
@@ -1,16 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEditorInternal.Profiling.Memory.Experimental.FileFormat;
 using UnityEngine;
+
+
+public enum BossState
+{
+    IDLE,
+    FOLLOW,
+    ATTACK,
+    DIE
+};
+
 
 public class BossBehaviour : MonoBehaviour
 {
+    public BossState bossState = BossState.IDLE;
 
     public bool playerPresent = false;
 
     GameObject player;
     private Animator animator;
     private Animation animation;
+
+
+    private float attackRange = 3.0f;
+    private bool inAttackCD = false;
+
+    float bulletSpeed = 10.0f;
+
+    int hp = 10;
+
+    [SerializeField]
+    private float attackCD = 1.0f;
+
+    public GameObject bulletPrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -23,13 +48,88 @@ public class BossBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (playerPresent && !animator.enabled)
+        // Continuously evaluate the player's distance and update bossState accordingly
+        if (playerPresent)
         {
-            animator.enabled = true; 
+            float distanceToPlayer = Vector3.Distance(transform.position, player.transform.position);
+
+            if (distanceToPlayer <= attackRange)
+            {
+                bossState = BossState.ATTACK;
+            }
+            //else
+            //{
+            //    bossState = BossState.FOLLOW; // Add FOLLOW logic here in the future
+            //}
         }
-        else if (!playerPresent && animator.enabled)
+        //else
+        //{
+        //    bossState = BossState.IDLE;
+        //}
+
+        // State behavior
+        switch (bossState)
         {
-            animator.enabled = false; 
+            case BossState.ATTACK:
+                Attack();
+                break;
+            case BossState.FOLLOW:
+
+                break;
+            case BossState.IDLE:
+
+                break;
+            case BossState.DIE:
+                Die();
+                break;
         }
+
+
+        animator.enabled = playerPresent;
+    }
+
+    private IEnumerator CoolDown()
+    {
+        inAttackCD = true;
+        yield return new WaitForSeconds(attackCD);
+        inAttackCD = false;
+    }
+    void Attack()
+    {
+
+        if (!inAttackCD)
+        {
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity) as GameObject;
+
+            Rigidbody2D rb = bullet.AddComponent<Rigidbody2D>();
+
+            Vector2 direction = (player.transform.position - transform.position).normalized;
+
+            rb.velocity = direction * bulletSpeed;
+
+            bullet.GetComponent<BaseProjectile>().GetPlayer(player.transform);
+            bullet.GetComponent<BaseProjectile>().isEnemyBullet = true;
+            StartCoroutine(CoolDown());
+
+        }
+    }
+
+
+    public void Die()
+    {
+        RoomsManager.instance.StartCoroutine(RoomsManager.instance.RoomCoroutine());
+        bossState = BossState.DIE;
+        Destroy(gameObject);
+    }
+
+    public void DamageBoss(int dmg)
+    {
+        hp -= dmg;
+
+        if (hp <= 0)
+        {
+            Die();
+        }
+
     }
 }
